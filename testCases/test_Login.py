f6c6c601cd9a6cc80fe18ed8b8b953ffd274d9e6
nitinsdcet <nitinsdcet@gmail.com>
2021-09-29 12:39:51 +0530

    fifte

--- testCases/test_Login.py
+++ testCases/test_Login.py
@@ -50,7 +50,7 @@ class Test_001_Login:
             self.driver.save_screenshot(".\\Screenshots\\"+"test_login.png")
             print(act_title)
             self.driver.close()
-            print("close brows")
+            print("close brow")
             assert False
 
 
